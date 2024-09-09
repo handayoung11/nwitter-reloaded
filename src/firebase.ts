d4380045ffc0023beefd0194b4a3b38e9d06299b
JinYeongKim <handayoung11@gmail.com>
2024-09-09 20:17:21 +0900

    firebase 설정 추가

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACnEMX6ZVAsO8E-Sd-mkRZp6kpAIYln3c",
+  authDomain: "nwitter-reloaded-8f212.firebaseapp.com",
+  projectId: "nwitter-reloaded-8f212",
+  storageBucket: "nwitter-reloaded-8f212.appspot.com",
+  messagingSenderId: "935124824347",
+  appId: "1:935124824347:web:02369dff437279b4c3e3be",
+};
+
+const app = initializeApp(firebaseConfig);
