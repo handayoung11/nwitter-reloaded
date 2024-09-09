8976b8fff3486e3124ad2d2ceba0d30d2936d1b0
JinYeongKim <handayoung11@gmail.com>
2024-09-09 20:03:48 +0900

    로딩화면 및 로그인, 회원가입, 프로파일, 인덱스 페이지 파일 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,66 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./component/layout";
+import Home from "./routes/home";
+import Proflie from "./routes/profile";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./component/loading-screen";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Proflie />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+  }
+`;
+
 function App() {
-  return <></>
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    //wait for firebase
+    setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  );
 }
 
-export default App
+export default App;
